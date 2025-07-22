bbcd223f1dc113b94d5e0f1c9ba2f891cdebd522
hello <1539829698@qq.com>
2025-07-22 21:55:58 +0800

    xxx

--- app/posts/[id]/page.jsx
+++ app/posts/[id]/page.jsx
@@ -7,20 +7,54 @@ export async function generateStaticParams() {
   return paths;
 }
 
+// 大纲组件
+function TableOfContents({ headings }) {
+  if (!headings || headings.length === 0) {
+    return null;
+  }
+
+  return (
+    <div className="toc-container">
+      <h3 className="toc-title">目录</h3>
+      <nav className="toc-nav">
+        <ul className="toc-list">
+          {headings.map((heading, index) => (
+            <li key={index} className={`toc-item toc-level-${heading.level}`}>
+              <a href={`#${heading.id}`} className="toc-link">
+                {heading.text}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
 export default async function Post({ params }) {
   const postData = await getPostData(params.id);
 
   return (
-    <div className='post-container'>
-      <h1>{postData.title}</h1>
-      <div className='post-meta'>
-        <span>{postData.date.toDateString()}</span>
-        {/* 若有阅读时长等其他元信息，也可放这里 */}
-      </div>
-      <div
-        className='post-content'
-        dangerouslySetInnerHTML={{ __html: postData.contentHtml }}
-      />
+    <div className="post-layout">
+      {/* 左侧大纲 */}
+      <aside className="post-sidebar">
+        <TableOfContents headings={postData.headings} />
+      </aside>
+      
+      {/* 主要内容 */}
+      <main className="post-main">
+        <div className='post-container'>
+          <h1>{postData.title}</h1>
+          <div className='post-meta'>
+            <span>{postData.date.toDateString()}</span>
+            {/* 若有阅读时长等其他元信息，也可放这里 */}
+          </div>
+          <div
+            className='post-content'
+            dangerouslySetInnerHTML={{ __html: postData.contentHtml }}
+          />
+        </div>
+      </main>
     </div>
   );
 }
