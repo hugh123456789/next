0eaefcaa3a9f6d0d609bb0ebffd8f3e327a12e02
hello <1539829698@qq.com>
2025-07-24 21:26:29 +0800

    xxxxx

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@
 
 export default function rootPage() {
     return (
-        <div  style={{color:'black',minWidth:'90vw'}}>
+        <div  style={{color:'black',minWidth:'90vw',height:'100vh',backgroundColor:'white'}}>
             <div className="w-full bg-white">xxx</div>
         </div>
     )
